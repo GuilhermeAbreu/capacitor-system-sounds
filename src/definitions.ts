cd4356abc69655911aca8613275e79c429a7a730
Guilherme Abreu <guilherme.abreu@klios.com.br>
2024-10-15 11:33:49 -0300

    tipagem capacitor

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,11 @@
-export interface CapacitorSystemSoundsPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+export interface SystemSoundsPlugin {
+  getSystemSounds(configSystemSound: {type: SystemSoundsType}): Promise<{ sounds: {title: string, uri: string}[] }>;
+  salveSoundLocation(systemSound: {uri: string, name: string}): Promise<{ filePath: string }>;
+}
+
+export enum SystemSoundsType {
+  RINGTONE = 1,    
+  NOTIFICATION = 2,
+  ALARM = 4,       
+  ALL = 7 
 }
